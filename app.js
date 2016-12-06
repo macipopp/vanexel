d59705e22ef8ecd0084d119d43b4ffdf789ff65a
dkoakes <doakes@expedia.com>
2016-12-06 11:14:15 -0600

    inital dev

---
+++ app.js
@@ -0,0 +1,189 @@
+var express = require('express');
+var exif = require('exif');
+var path = require('path');
+var fs = require("fs");
+var gm = require('gm');
+var sizeOf = require('image-size');
+var Twit = require('twit');
+var config = require('./config');
+var FB = require('fb');
+var request = require('request');
+
+var T = new Twit({
+  consumer_key:         config.twitter.consumer_key,
+  consumer_secret:      config.twitter.consumer_secret,
+  access_token:         config.twitter.access_token,
+  access_token_secret:  config.twitter.access_token_secret,
+  timeout_ms:           config.twitter.timeout_ms,  // optional HTTP request timeout to apply to all requests.
+})
+
+
+var app = express();
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Watch directory for changes then perform some action
+require('chokidar').watch('./photos', {ignored: /[\/\\]\./}).on('add', function(event, photoPath) {
+  var basename = path.basename(event);
+  var watermarkX = 0;
+  var watermarkY = 0;
+  var watermarkPath = __dirname + config.paths.watermark_basename;
+  var watermarkDirectoryPath = __dirname + config.paths.watermark_directory;
+  var ExifImage = exif.ExifImage;
+  var fbAlbums = [];
+  var fbAlbumId;
+
+    try {
+        new ExifImage({ image : __dirname + '/photos/'+basename }, function (error, exifData) {
+            if (error){
+                console.log('Error: '+error.message);
+            }
+            else {
+                // Do something with your data!
+                var dimensions = sizeOf(watermarkPath);
+                // determine image orientation and size
+                if (isPhotoOrientationPortrait(exifData)) {
+                    watermarkX = 0;
+                    watermarkY = exifData.exif.ExifImageHeight - (dimensions.height + 100);
+                    var watermarkPortraitPath = buildPortraitWatermarkPath();
+                    gm(watermarkPath).rotate('green',90).write(watermarkPortraitPath, function(e){
+                        var command = [];
+                        command.push("image Over ", watermarkX, ",", watermarkY, " 0,0 ", watermarkPortraitPath);
+                        var graphicMagicCommand = command.join("");
+                        addWatermarkToPhoto(graphicMagicCommand, watermarkPortraitPath);
+                    });
+                } else if (isPhotoOrientationLandscape(exifData)) {
+                    console.log('photo is landscape');
+                    watermarkX = exifData.exif.ExifImageWidth - (dimensions.width + 100);
+                    watermarkY = exifData.exif.ExifImageHeight - (dimensions.height + 100);
+                    var command = [];
+                    command.push("image Over ", watermarkX, ",", watermarkY, " 0,0 ", watermarkPath);
+                    var graphicMagicCommand = command.join("");
+                    addWatermarkToPhoto(graphicMagicCommand, watermarkPath);
+                }
+            }
+        });
+    } catch (error) {
+        console.log('Error: ' + error.message);
+    }
+
+    function isPhotoOrientationPortrait(exifData) {
+        return exifData.image.Orientation == 8 || exifData.image.Orientation == 4;
+    }
+
+    function isPhotoOrientationLandscape(exifData) {
+        return exifData.image.Orientation == 1 || exifData.image.Orientation == 5;
+    }
+
+    function buildPortraitWatermarkPath() {
+        var watermarkPortraitBaseNameTmp = path.basename(watermarkPath, path.extname(watermarkPath));
+        var watermarkPortraitBaseName = watermarkPortraitBaseNameTmp + 'Portrait';
+        var watermarkPortraitPath = __dirname + "/" + watermarkPortraitBaseName + path.extname(watermarkPath);
+        return watermarkPortraitPath;
+    }
+
+    function addWatermarkToPhoto(command, watermarkPortraitPath) {
+        gm(__dirname + '/photos/' + basename)
+            .draw(command)
+            .write('' + __dirname + '/waterMarkedPhotos/' + basename, function (e) {
+               // postMediaToTwitter();
+                //postMediaToFacebook();
+                console.log('watermarked: '+ basename);
+                console.log('watermarked: '+ basename);
+                console.log('' + __dirname + '/waterMarkedPhotos/' + basename);
+            });
+    }
+
+    function postMediaToTwitter() {
+        var b64content = fs.readFileSync(watermarkDirectoryPath + basename, {encoding: 'base64'})
+
+        T.post('media/upload', {media_data: b64content}, function (err, data, response) {
+            // now we can assign alt text to the media, for use by screen readers and
+            // other text-based presentations and interpreters
+            var mediaIdStr = data.media_id_string
+            var altText = "Some photo that I uploaded"
+            var meta_params = {media_id: mediaIdStr, alt_text: {text: altText}}
+
+            T.post('media/metadata/create', meta_params, function (err, data, response) {
+                if (!err) {
+                    // now we can reference the media and post a tweet (media will attach to the tweet)
+                    var params = {status: config.twitter.hashtag, media_ids: [mediaIdStr]}
+
+                    T.post('statuses/update', params, function (err, data, response) {
+                        console.log(data)
+                    })
+                }
+            })
+        })
+    }
+
+    function postMediaToFacebook() {
+        FB.api('oauth/access_token', {
+            client_id: config.facebook.app_token,
+            client_secret: config.facebook.app_secret,
+            grant_type: 'client_credentials'
+        }, function (res) {
+            if(!res || res.error) {
+                console.log(!res ? 'error occurred' : res.error);
+                return;
+            }
+
+            //var accessToken = res.access_token;
+            FB.setAccessToken(config.facebook.access_token);
+            //sendToFb();
+        });
+    }
+
+
+    function sendToFb() {
+        FB.api(
+            "me/albums", 'post', {name: config.facebook.album_name, message: config.facebook.album_description},
+            function (response) {
+                if(!response || response.error) {
+                    console.log(!response ? 'error occurred' : response.error);
+                    return;
+                }
+                if (response && !response.error) {
+                    console.log('Album Id for this event:'+response.id);
+                    fbAlbumId = response.id;
+                }
+
+                FB.api(
+                    "me/albums",
+                    function (response) {
+                        if (response && !response.error) {
+                            for (var name in response.data) {
+                                if (response.data.hasOwnProperty(name)) {
+                                    var val = response.data[name];
+                                    console.log(val.id);
+                                    fbAlbums.push(val.id);
+                                }
+                            }
+                        }
+
+
+                        FB.api(''+fbAlbumId+'/photos', 'post', { source: fs.createReadStream(watermarkDirectoryPath + basename), caption: config.facebook.caption }, function (res) {
+                            if(!res || res.error) {
+                                console.log(!res ? 'error occurred' : res.error);
+                                return;
+                            }
+                            console.log('Post Id: ' + res.post_id);
+                        });
+                    }
+                );
+            }
+        );
+    }
+});
+
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,26 @@
+var config = {};
+config.paths = {};
+config.twitter = {};
+config.facebook = {};
+
+config.paths.watermark_basename = '/original.png';
+config.paths.watermark_directory = '/waterMarkedPhotos/'
+
+config.twitter.consumer_key = 'Evei2dSBdROuCf8YMKGs4zeV0',
+config.twitter.consumer_secret = 'YXweP4KoZcKkpciAYCo2FIu1HZUTAw3Se0T8CLv4WCywzTInGA',
+config.twitter.access_token = '804111556174823424-Z0jDcBmgX5fdQTqQpLOvTM0QLXec08l',
+config.twitter.access_token_secret =  'iiTBEHvVbE34GPKW5QD5u4FXPFETQgzcHMUOVk8tuBdtt',
+config.twitter.timeout_ms = 60*1000,  // optional HTTP request timeout to apply to all requests.
+
+config.twitter.hashtag = '#officeLife';
+
+config.facebook.user_token = '1802633836646172';
+config.facebook.app_token = '1802633836646172';
+config.facebook.app_secret = 'af617f6cf383116e4811108ba06c520b';
+config.facebook.access_token = 'EAAZAnfGVZBTxwBAHfP9DCeTcRoKrQEaZBY1ZBZCrvhEYjiPuvfZAhX8IZABbowQMlJ7CIdkezHKGKvpeF9TOENOCFgFMHngsZBLtqFQkEKkxuBENNsziM8eZBc3o73XQ7PU3e5rOroav8ISZBYZBFXKZACbzjdiOWQcM3HZC3ZBeQQ48UmGWvLYaL9a7RA';
+config.facebook.caption = 'testing facebook';
+config.facebook.album_name = 'nye 2017'
+config.facebook.album_description = 'be in the best place in the chi when the ball drops';
+
+
+module.exports = config;
