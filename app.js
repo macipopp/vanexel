529c66deb524d6a00a4f4770df20e67bc57b1691
dkoakes <doakes@expedia.com>
2016-12-11 01:12:28 -0600

    body parser for console validation

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ var T = new Twit({
 
 
 var app = express();
-//var bodyParser = require('body-parser')
-//app.use( bodyParser.json() );       // to support JSON-encoded bodies
-//app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//    extended: true
-//}));
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 //app.use(express.static(__dirname + "public"));
 
 //Setting default template
